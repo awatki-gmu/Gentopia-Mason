ba77d8ad31260dc096bf187954777f8e03e54e93
Lexy <lexy@Lexys-Air.lan>
2024-10-09 13:57:21 -0400

    first commit

--- GentPool/gentpool/pool/__init__.py
+++ GentPool/gentpool/pool/__init__.py
@@ -9,3 +9,5 @@ from .vanilla_template import *
 from .openai_memory_template import *
 from .vanilla-ziyu import *
 from .scholar-ziyu import *
+from .vanilla_agent import *
+from .scholar_agent import *
